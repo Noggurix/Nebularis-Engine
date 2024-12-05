0864efea44732d6277c6180d7a3c00d00f482aee
João Gabriel <gabrielnew693@gmail.com>
2024-12-05 00:20:27 -0300

    New features added
    Neptune, Uranus, Jupiter, Saturn with its rings, and Earth's moon have been added, along with a correction to the lighting and code.

--- js/cameraControl.js
+++ js/cameraControl.js
@@ -5,42 +5,44 @@ export default class CameraControls {
         this.camera = camera;
         this.domElement = domElement;
 
-        this.isDragging = false; // Rastreamento de arrasto do mouse
-        this.prevMousePosition = { x: 0, y: 0 }; // Posição anterior do mouse para calculo de deslocamento
-        this.cameraControlRotation = { x: 0, y: 0 }; // Estado interno de rotação
-        this.cameraSpeed = 0.2;
+        this.isDragging = false;
+        this.previousMousePosition = { x: 0, y: 0 };
+        this.cameraControlRotation = { x: 0, y: 0 };
+        this.cameraSpeed = 0.9;
 
-        this.keys = { w: false, a: false, s: false, d: false }; // Teclas de movimentação
+        this.keys = { w: false, a: false, s: false, d: false };
 
         this.initEventListeners();
     }
 
     initEventListeners() {
+
         // Eventos do mouse
         this.domElement.addEventListener('mousedown', (event) => {
-            if (event.button === 0) this.isDragging = true; // Se o botão for clicado isDragging se torna True
-            this.prevMousePosition = { x: event.clientX, y: event.clientY }; // Guardar posição atual do mouse
+            if (event.button === 0) this.isDragging = true;
+            this.previousMousePosition = { x: event.clientX, y: event.clientY };
         });
         this.domElement.addEventListener('mouseup', () => {
-            this.isDragging = false; // Se não houver clique do botão desativa isDragging
+            this.isDragging = false;
         });
         this.domElement.addEventListener('mousemove', (event) => {
             if (this.isDragging) {
                 // Diferenças entre a posição atual e anterior
-                const deltaX = event.clientX - this.prevMousePosition.x;
-                const deltaY = event.clientY - this.prevMousePosition.y;
+                const deltaX = event.clientX - this.previousMousePosition.x;
+                const deltaY = event.clientY - this.previousMousePosition.y;
 
                 // Atualiza a rotação da câmera com base no movimento do mouse
                 this.cameraControlRotation.y -= deltaX * 0.005; // Rota horizontalmente com base na diferença de posição de deltaX
-                this.cameraControlRotation.x -= deltaY * 0.005; // Rota verticalmente com base na diferença de posição de deltaY
+                this.cameraControlRotation.x -= deltaY * 0.005;
 
-                // Limita a rotação no eixo X entre -PI/2 e PI/2 (impede que a câmera "vire de cabeça para baixo")
+                // Limita a rotação do eixo X entre -PI/2 e PI/2 (impede que a câmera "vire de cabeça para baixo")
                 this.cameraControlRotation.x = Math.max(-Math.PI / 2, Math.min(Math.PI / 2, this.cameraControlRotation.x));
 
-                this.prevMousePosition = { x: event.clientX, y: event.clientY }; // Guarda novamente a posição atual do mouse
+                this.previousMousePosition = { x: event.clientX, y: event.clientY };
             }
         });
-        // Eventos de teclado para movimentação
+
+        // Eventos do teclado
         this.domElement.addEventListener('keydown', (event) => {
             this.keys[event.key.toLowerCase()] = true;
         });
@@ -49,48 +51,37 @@ export default class CameraControls {
         });
     }
 
-    /**
-     * Atualiza a posição da câmera com base nas teclas pressionadas.
-     */
-    updatePosition() {
-        const forwardBackward = new THREE.Vector3(); // Vetor para movimentos frente/trás
-        const lateral = new THREE.Vector3(); // Vetor para movimentos laterais
-        const up = new THREE.Vector3(); // Vetor para movimentos verticais
+    callCameraUpdates() {
+        this.updateCameraPosition();
+        this.updateCameraRotation();
+    }
 
-        // Movimentos laterais (X)
-        if (this.keys.d) lateral.x -= this.cameraSpeed;
-        if (this.keys.a) lateral.x += this.cameraSpeed;
+    updateCameraPosition() {
+        const forwardBackward = new THREE.Vector3();
+        const lateral = new THREE.Vector3();
+        const up = new THREE.Vector3();
 
-        // Movimentos para frente/trás (Z)
         if (this.keys.w) forwardBackward.z += this.cameraSpeed;
         if (this.keys.s) forwardBackward.z -= this.cameraSpeed;
+        if (this.keys.a) lateral.x += this.cameraSpeed;
+        if (this.keys.d) lateral.x -= this.cameraSpeed;
 
-        // Calcular o movimento de altura (Y)
-        const verticalMovementFactor = Math.sin(this.cameraControlRotation.x); // Isso cria um movimento contínuo baseado na inclinação
-        if (this.keys.w) up.y += this.cameraSpeed * verticalMovementFactor; // Se W estiver pressionado, move Y para cima ou baixo dependendo de verticalMovementFactor
-        if (this.keys.s) up.y -= this.cameraSpeed * verticalMovementFactor; // Mesmo comportamento acima porém para a tecla S
+        // Mover Y
+        const verticalMovementFactor = Math.sin(this.cameraControlRotation.x); // Cria um movimento contínuo baseado na inclinação
+        if (this.keys.w) up.y += this.cameraSpeed * verticalMovementFactor;
+        if (this.keys.s) up.y -= this.cameraSpeed * verticalMovementFactor;
 
-        // Aplica a rotação da câmera aos vetores de movimento
         forwardBackward.applyEuler(new THREE.Euler(0, this.cameraControlRotation.y, 0));
         lateral.applyEuler(new THREE.Euler(0, this.cameraControlRotation.y, 0));
 
-        // Atualiza a posição da câmera
         this.camera.position.add(forwardBackward).add(lateral).add(up);
     }
 
-    /**
-     * Atualiza a rotação da câmera para olhar na direção desejada.
-     */
-    updateRotation() {
+    updateCameraRotation() {
         this.camera.lookAt(
             this.camera.position.x + Math.sin(this.cameraControlRotation.y),
             this.camera.position.y + Math.sin(this.cameraControlRotation.x),
             this.camera.position.z + Math.cos(this.cameraControlRotation.y)
         );
-    }
-
-    update() {
-        this.updatePosition();
-        this.updateRotation();
     }
 }
--- js/main.js
+++ js/main.js
@@ -5,14 +5,11 @@ import { EffectComposer } from 'EffectComposer';
 import { RenderPass } from 'RenderPass';
 import CameraControls from './cameraControl.js';
 
-// stats.js (Medidor de performance)
-const stats = new Stats();
-stats.showPanel(0);
-document.body.appendChild(stats.dom);
+const performanceStats = new Stats();
+performanceStats.showPanel(0);
+document.body.appendChild(performanceStats.dom);
 
-// Configurações gerais
 const CONFIG = {
-  scaleFactor: 60,
   camera: { fov: 50, aspect: window.innerWidth / window.innerHeight, near: 0.1, far: 2000 },
   renderer: { antialias: true },
   bloom: { strength: 0.8, radius: 0.9, threshold: 0.1 },
@@ -21,32 +18,33 @@ const CONFIG = {
     "skybox/posY.jpg", "skybox/negY.jpg",
     "skybox/posZ.jpg", "skybox/negZ.jpg"
   ],
+  planetScaleFactor: 100,
   planets: [
-    { name: "Mercury", texture: "textures/mercury/mercury.jpg", distance: 0.39, size: 0.5, period: 88 },
-    { name: "Venus", texture: "textures/venus/venus.jpg", distance: 0.72, size: 1, period: 225 },
-    { name: "Earth", texture: "textures/earth/earth_daymap.jpg", distance: 1.0, size: 1.2, period: 365.25 },
-    { name: "Mars", texture: "textures/mars/mars.jpg", distance: 1.52, size: 0.8, period: 687 },
-  ],
+    { name: "Mercury", texture: "textures/mercury/mercury.jpg", distance: 0.387, size: 0.5, period: 88 },
+    { name: "Venus", texture: "textures/venus/venus.jpg", distance: 0.722, size: 0.9, period: 225 },
+    { name: "Earth", texture: "textures/earth/earth_daymap.jpg", distance: 1.00, size: 1.5, period: 365.25 },
+    { name: "Mars", texture: "textures/mars/mars.jpg", distance: 1.52, size: 0.77, period: 687 },
+    { name: "Jupiter", texture: "textures/jupiter/jupiter.jpg", distance: 5.2, size: 14.5, period: 4333 },
+    { name: "Saturn", texture: "textures/saturn/saturn.jpg", distance: 9.58, size: 12.5, period: 10759 },
+    { name: "Uranus", texture: "textures/uranus/uranus.jpg", distance: 14.2, size: 3.87, period: 30687 },   
+    { name: "Neptune", texture: "textures/neptune/neptune.jpg", distance: 20.1, size: 3.69, period: 60190 }
+  ]
 };
 
-// Configuração da cena
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(CONFIG.camera.fov, CONFIG.camera.aspect, CONFIG.camera.near, CONFIG.camera.far);
 const renderer = new THREE.WebGLRenderer(CONFIG.renderer);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Carregar skybox
-const loader = new THREE.CubeTextureLoader();
-scene.background = loader.load(CONFIG.skybox);
+const skyboxLoader = new THREE.CubeTextureLoader();
+scene.background = skyboxLoader.load(CONFIG.skybox);
 
-// Controles da câmera
-const cameraControls = new CameraControls(camera, document); // Carrega o script de controle de câmera
+const camera = new THREE.PerspectiveCamera(CONFIG.camera.fov, CONFIG.camera.aspect, CONFIG.camera.near, CONFIG.camera.far);
+const cameraControls = new CameraControls(camera, document);
 camera.position.set(0, 50, 100);
 cameraControls.cameraControlRotation.x = -Math.PI / 6;
 cameraControls.cameraControlRotation.y = Math.atan2(-camera.position.x, -camera.position.z);
 
-// Pós-processamento
 const composer = new EffectComposer(renderer);
 composer.addPass(new RenderPass(scene, camera));
 composer.addPass(new UnrealBloomPass(
@@ -54,51 +52,106 @@ composer.addPass(new UnrealBloomPass(
   CONFIG.bloom.strength, CONFIG.bloom.radius, CONFIG.bloom.threshold
 ));
 
-//Luz do sol
-const sunLight = new THREE.PointLight(0xffffff, 500);
+const sunLight = new THREE.PointLight(0xffffff, 0.3, 10000, 0);
 sunLight.position.set(0, 0, 0);
 scene.add(sunLight);
 
-// Carregador de texturas
 const textureLoader = new THREE.TextureLoader();
 
-// Adiciona Sol
-const sunMaterial = new THREE.MeshBasicMaterial({ map: textureLoader.load("textures/sun/sun.jpg") }); // Material do sol
-const sun = new THREE.Mesh(new THREE.SphereGeometry(10, 64, 64), sunMaterial); // Objeto sol com sua geometria e material
+const sunMaterial = new THREE.MeshBasicMaterial({ map: textureLoader.load("textures/sun/sun.jpg") });
+const sun = new THREE.Mesh(new THREE.SphereGeometry(30, 64, 64), sunMaterial);
 scene.add(sun);
 
-// Adiciona planetas
-const planets = CONFIG.planets.map(data => {
-  const planetMaterial = new THREE.MeshStandardMaterial({ map: textureLoader.load(data.texture) });
-  const planet = new THREE.Mesh(new THREE.SphereGeometry(data.size, 64, 64), planetMaterial); // Objeto planeta com geometria e material
-  planet.userData = {
-    distance: data.distance * CONFIG.scaleFactor,
+const planetIteration = CONFIG.planets.map(data => {
+  const planetMaterial = new THREE.MeshStandardMaterial({ map: textureLoader.load(data.texture)});
+  const objPlanet = new THREE.Mesh(new THREE.SphereGeometry(data.size, 64, 64), planetMaterial);
+  objPlanet.name = data.name;
+
+  if (data.name === "Earth") {
+    const moon = createMoon(textureLoader);
+
+    const moonOrbitGroup = new THREE.Group();
+    moonOrbitGroup.add(moon);
+    objPlanet.add(moonOrbitGroup);
+
+    moonOrbitGroup.userData = {
+      orbitRadius: 0.02 * CONFIG.planetScaleFactor,
+      angle: Math.random() * Math.PI * 2,
+      speed: (2 * Math.PI) / 27.3 / 100
+    };
+  }
+
+  if (data.name === "Saturn") {
+    const saturnRing = createSaturnRings(textureLoader);
+
+    const ringOrbit = new THREE.Group();
+    ringOrbit.add(saturnRing);
+    objPlanet.add(ringOrbit);
+  }
+
+  objPlanet.userData = {
+    distance: data.distance * CONFIG.planetScaleFactor,
     angle: Math.random() * Math.PI * 2,
     speed: (2 * Math.PI) / data.period / 100
   };
-  scene.add(planet);
-  return planet;
+
+  scene.add(objPlanet);
+
+  return objPlanet;
 });
 
-// Animação
-function animate() {
-  stats.begin();
-  
-  cameraControls.update();
+function createMoon(textureLoader) {
+  const moonMaterial = new THREE.MeshStandardMaterial({ map: textureLoader.load("textures/moon/moon.jpg") });
+  const objMoon = new THREE.Mesh(new THREE.SphereGeometry(0.32, 64, 64), moonMaterial);
 
-  planets.forEach(planet => {
-    const data = planet.userData;
-    data.angle += data.speed; // Atualiza o ângulo de rotação
-    planet.position.x = Math.cos(data.angle) * data.distance; // X baseado no coseno
-    planet.position.z = Math.sin(data.angle) * data.distance; // Z baseado no seno
+  return objMoon;
+}
+
+function createSaturnRings (textureLoader) {
+  const saturnRingGeometry = new THREE.RingGeometry(15.5, 21.5, 128);
+  const saturnRingMaterial = new THREE.MeshBasicMaterial({
+    map: textureLoader.load("textures/saturn/saturn_ring.png"),
+    side: THREE.DoubleSide,
+    opacity: 0.4,
+    transparent: true,
+    color: 0x888888
   });
 
-  composer.render(); // Desenha a cena
-  stats.end();
+  const saturnRing = new THREE.Mesh(saturnRingGeometry, saturnRingMaterial);
+  saturnRing.rotation.x = 20;
+  saturnRing.rotation.z = 10;
+
+  return saturnRing;
+}
+
+function animate() {
+  performanceStats.begin();
+
+  cameraControls.callCameraUpdates();
+
+  planetIteration.forEach(objPlanet => {
+    const data = objPlanet.userData;
+    data.angle += data.speed;
+    objPlanet.position.x = Math.cos(data.angle) * data.distance; // X baseado no coseno
+    objPlanet.position.z = Math.sin(data.angle) * data.distance; // Z baseado no seno
+
+    if (objPlanet.name === "Earth" && objPlanet.children.length > 0) {
+      const moonOrbitGroup = objPlanet.children[0];
+      const moonData = moonOrbitGroup.userData;
+
+      moonData.angle += moonData.speed;
+      moonOrbitGroup.position.x = Math.cos(moonData.angle) * moonData.orbitRadius;
+      moonOrbitGroup.position.z = Math.sin(moonData.angle) * moonData.orbitRadius;
+    }
+  });
+
+  composer.render();
+
+  performanceStats.end();
+
   requestAnimationFrame(animate);
 }
 
-// Ajustar o canvas ao redimensionar
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
